05278a49a444e071ca8cd0b40e9c74b20927bcd0
Patri <patriciafornielesro@gmail.com>
2024-11-05 11:04:10 +0100

    Tasca_1-N1

---
+++ S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/Controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package cat.itacademy.s04.t01.n01.Controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldController {
+    @GetMapping(value = { "/HelloWorld" })
+    @ResponseBody
+    public String greet(@RequestParam(value = "name", defaultValue = "UNKNOWN") String name){
+        return String.format("Hello %s", name) + ", you are executing a Maven project";
+    }
+
+    @GetMapping(value = { "/HelloWorld2", "/HelloWorld2/{name}" })
+    @ResponseBody
+    public static String greet2(@PathVariable (value = "name", required = false) String name) {
+        name = name!= null ? name : "UNKNOWN";
+        return String.format("Hello %s", name) + ", you are executing a Maven project";
+    }
+}
