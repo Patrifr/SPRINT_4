8e5f1523ab96910e6d6527bf265151f6c18edd84
Patri <patriciafornielesro@gmail.com>
2024-11-06 13:05:16 +0100

    Tasca_1

--- S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/Controller/HelloWorldController.java
+++ S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/Controller/HelloWorldController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @SpringBootApplication
 public class HelloWorldController {
+
     @GetMapping(value = { "/HelloWorld" })
     @ResponseBody
     public String greet(@RequestParam(value = "name", defaultValue = "UNKNOWN") String name){
@@ -14,7 +15,7 @@ public class HelloWorldController {
 
     @GetMapping(value = { "/HelloWorld2", "/HelloWorld2/{name}" })
     @ResponseBody
-    public static String greet2(@PathVariable (value = "name", required = false) String name) {
+    public static String greet2(@PathVariable(value = "name", required = false) String name) {
         name = name!= null ? name : "UNKNOWN";
         return String.format("Hello %s", name) + ", you are executing a Maven project";
     }
--- S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/S04T01N01Application.java
+++ S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/S04T01N01Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class S04T01N01Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(S04T01N01Application.class, args);
 	}
+
 }
