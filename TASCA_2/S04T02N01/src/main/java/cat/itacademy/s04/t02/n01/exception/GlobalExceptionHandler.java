5a4f97c273f7f83b9f8f5dae93dedbf134910182
Patri <patriciafornielesro@gmail.com>
2024-11-12 12:03:10 +0100

    Tasca_2_N1

---
+++ TASCA_2/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package cat.itacademy.s04.t02.n01.exception;
+
+import cat.itacademy.s04.t02.n01.exception.CustomExceptions.FruitExistsException;
+import cat.itacademy.s04.t02.n01.exception.CustomExceptions.FruitNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FruitNotFoundException.class)
+    public ResponseEntity<String> handleFruitNotFoundException(FruitNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FruitExistsException.class)
+    public ResponseEntity<String> handleFruitExistsException(FruitExistsException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e){
+        return new ResponseEntity<>("Unexpected error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ TASCA_2/S04T02N01/src/main/java/cat/itacademy/s04/t02/n01/services/Impl/FruitServiceImpl.java
@@ -0,0 +1,59 @@
+package cat.itacademy.s04.t02.n01.services.Impl;
+
+import cat.itacademy.s04.t02.n01.exception.CustomExceptions.FruitExistsException;
+import cat.itacademy.s04.t02.n01.exception.CustomExceptions.FruitNotFoundException;
+import cat.itacademy.s04.t02.n01.model.Fruit;
+import cat.itacademy.s04.t02.n01.repository.FruitRepository;
+import cat.itacademy.s04.t02.n01.services.FruitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class FruitServiceImpl implements FruitService {
+    @Autowired
+    private FruitRepository fruitRepository;
+
+    @Override
+    public Fruit addFruit(Fruit fruit){
+        fruitRepository.findByName(fruit.getName()).ifPresent(dupFruit ->{
+            throw new FruitExistsException("Error. " + fruit.getName() + " already exists.");
+        });
+        return fruitRepository.save(fruit);
+    }
+
+    @Override
+    public Fruit updateFruit(Fruit fruit){
+        Fruit fruitToUpdate = fruitRepository.findById(fruit.getId()).orElseThrow(() ->
+                new FruitNotFoundException("Error while updating. Fruit with id " + fruit.getId() +
+                        " not found."));
+
+        fruitToUpdate.setName(fruit.getName());
+        fruitToUpdate.setQuantityKg(fruit.getQuantityKg());
+
+        return fruitRepository.save(fruitToUpdate);
+    }
+
+    @Override
+    public void deleteFruit(int id){
+        findFruitById(id);
+        fruitRepository.deleteById(id);
+    }
+
+    @Override
+    public Fruit getOneFruit(int id) {
+        return findFruitById(id);
+    }
+
+    @Override
+    public List<Fruit> getAllFruits(){
+        return  fruitRepository.findAll();
+    }
+
+    private Fruit findFruitById(int id) {
+        return fruitRepository.findById(id)
+                .orElseThrow(() -> new FruitNotFoundException("Fruit with id " + id + " not found."));
+    }
+}
